dcc5596f650c456b02cef835d46e530d1181bf0e
OGM <walisonmichael8@gmail.com>
2025-09-12 08:33:32 +0100

    commits

---
+++ server/routes/route_pages.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controll_pages =require('../controller/controll_pages');
+
+
+router.get('/',controll_pages.home);
+router.get('/staking',controll_pages.staking);
+router.get('/howtobuy',controll_pages.howtobuy);
+router.get('/token',controll_pages.token);
+
+module.exports =router;
