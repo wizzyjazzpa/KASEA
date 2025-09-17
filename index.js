e3267b0b354fbeb3498b003fcf953335d73079c2
OGM <walisonmichael8@gmail.com>
2025-09-17 17:07:48 +0100

    thirdweb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 
 const app = express();
