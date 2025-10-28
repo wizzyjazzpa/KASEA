fd41abef27756b157fb1adcc92b5eaeef91eca1e
OGM <walisonmichael8@gmail.com>
2025-10-28 21:27:28 +0100

    addd

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const initPhase = require('./server/middleware/initializePhase');
 
 
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT;
 //database connection
 
 connectDB();
